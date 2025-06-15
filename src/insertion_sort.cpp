225972d4fecf66414222014aa2f99e3021ce4640
devIncogni <arinkumar10@outlook.com>
2025-06-16 01:21:26 +0530

    Better insertion sort

--- src/insertion_sort.cpp
+++ src/insertion_sort.cpp
@@ -3,12 +3,20 @@
 
 void insertionSort(std::vector<int> &array) {
   for (int i{1}; i < array.size(); ++i) {
-    int positionOfCurrentElement{i};
-    for (int j{i - 1}; j >= 0; --j) {
-      if (array[positionOfCurrentElement] < array[j]) {
-        std::swap(array[positionOfCurrentElement], array[j]);
-        positionOfCurrentElement = j;
-      }
-    }
+    int currentElement{array[i]};
+    // for (int j{i - 1}; j >= 0; --j) {
+    //   if (array[positionOfCurrentElement] < array[j]) {
+    //     std::swap(array[positionOfCurrentElement], array[j]);
+    //     positionOfCurrentElement = j;
+    //   }
+    // }
+    int j{i - 1};
+    while (j >= 0 && array[j] > currentElement) {
+      // Keep shifting one position fwd
+      array[j + 1] = array[j];
+      --j;
+    } // After every shift, j and j+1 will have duplicates since j was shifted
+      // to j+1. Hence now put current element at j.
+    array[j + 1] = currentElement;
   }
 }
